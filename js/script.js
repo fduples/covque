64b0d05c1c279f7f0ed08fce033a851e858e4b8e
Fade-tor <72475945+Fade-tor@users.noreply.github.com>
2025-02-14 14:47:55 -0300

    final modelo para presentar

--- js/script.js
+++
@@ -1,4 +0,0 @@
-// Puedes agregar interactividad con JavaScript aquí
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('La página ha cargado correctamente.');
-});
---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// Resalta el dia actual en la store
+window.addEventListener('DOMContentLoaded', event => {
+    const listHoursArray = document.body.querySelectorAll('.list-hours li');
+    listHoursArray[new Date().getDay()].classList.add(('today'));
+})
